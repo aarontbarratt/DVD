4d9e4e8cf3e4e86a0039d499e034b9552e20e9ff
Aaron Barratt <aarontbarratt@gmail.com>
2019-04-24 12:24:03 +0100

    Removed add item tests from inventory.py

--- inventory.py
+++ inventory.py
@@ -12,9 +12,3 @@ class Inventory:
     def get_items(self):
         for item in self.slots:
             print(str(item.quantity) + 'x : ' + item.name.title() + ', ' + item.description)
-
-
-bag = Inventory()
-bag.add_item('key', 'a key for a door')
-bag.add_item('umbrella', 'under my um-ber-ella')
-bag.get_items()
