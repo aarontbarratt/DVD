8e656855e4e3809872ac272e42268b838c1c8114
Aaron Barratt <aarontbarratt@gmail.com>
2019-04-30 15:27:14 +0100

    check_connection function now works!

--- test.py
+++ test.py
@@ -2,25 +2,48 @@ from world import Room
 
 
 def print_array(array):
-    for row in array:
-        for column in row:
-            if type(column) is Room:
-                print(column.name, end=' ')
+    for column in array:
+        for row in column:
+            if type(row) is Room:
+                print(row.name, end=' ')
             else:
-                print(column, end=' ')
+                print(row, end=' ')
         print()  # prints new line
+
+
+def check_connected(array, x, y):
+    temp = array
+    temp[y][x] = 'H'
+    print_array(temp)
+
+    result = []
+
+    if y + 1 <= size - 1:
+        if temp[y + 1][x] != 0:
+            result.append(temp[y + 1][x])
+
+    if x + 1 <= size - 1:
+        if temp[y][x + 1] != 0:
+            result.append(temp[y][x + 1])
+
+    if y - 1 >= 0:
+        if temp[y - 1][x] != 0:
+            result.append(temp[y - 1][x])
+
+    if x - 1 >= 0:
+        if temp[y][x - 1] != 0:
+            result.append(temp[y][x - 1])
+
+    return result
 
 
 size = 5
 a = [[0] * size for i in range(size)]
 
-living_room = Room('Living Room')
+a[0][0] = 1
+a[1][0] = 2
+a[0][1] = 3
+a[4][4] = 5
 
-a[4][2] = 1
-a[3][2] = 2
-a[2][2] = 3
-a[2][1] = 4
-a[2][0] = 5
-a[1][0] = 6
-a[2][3] = 7
-print_array(a)
+check = check_connected(a, 2, 2)
+print(check)
